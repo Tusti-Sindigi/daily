2df42cb482121ae8dce3bfadb3920b1e57fe3687
Tusti Sindigi <tustiisindigi@gmail.com>
2026-06-21 08:56:14 +0530

    maximum ice cream bars sol 2

--- maximum-ice-cream-bars.cpp
+++ maximum-ice-cream-bars.cpp
@@ -6,14 +6,29 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) 
     {
-        sort(costs.begin(), costs.end());
+        int greatest=*max_element(costs.begin(), costs.end());
+        vector<int> freq(greatest+1, 0);
+
+        for(int c: costs)
+            freq[c]++;
+
+        for(int i=1;i<freq.size();i++)
+            freq[i]+=freq[i-1];
+
+        vector<int> sorted(costs.size(), 0);
+
+        for(int i=costs.size()-1;i>=0;i--)
+        {
+            sorted[freq[costs[i]]-1]=costs[i];
+            freq[costs[i]]--;
+        }    
 
         int i=0, max=0;
 
-        while(i<costs.size() && coins>=costs[i])
+        while(i<sorted.size() && coins>=sorted[i])
         {
             max++;
-            coins-=costs[i];
+            coins-=sorted[i];
             i++;
         }
         return max;
